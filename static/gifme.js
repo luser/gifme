ba013dbb8152afc248e142c58674d3ee8209ed73
Ted Mielczarek <ted@mielczarek.org>
2015-02-21 12:50:42 -0500

    Display first GIF immediately

--- static/gifme.js
+++ static/gifme.js
@@ -23,23 +23,32 @@ function preloadNextImage() {
   loading.onload = function() {
     queue.shift();
     console.log("loaded %s", loading.src);
-    gifs.push(loading);
-    loading = null;
-    if (current_gif == -1) {
-      loadNextGif();
+    if (!loading.displayed) {
+      gifs.push(loading);
     }
+    loading = null;
     setTimeout(preloadNextImage, 0);
   };
   loading.src = queue[0];
   console.log("preloading %s", loading.src);
+  if (current_gif == -1) {
+    gifs.push(loading);
+    loading.displayed = true;
+    loadNextGif();
+  }
 }
 
 function loadNextGif() {
   if (current_gif == -1) {
     current_gif = 0;
   } else {
+    if (gifs.length == 1) {
+      // Only one gif, don't bother changing.
+      return;
+    }
     current_gif = (current_gif + 1) % gifs.length;
   }
+  console.log("using gif %s", gifs[current_gif].src);
   document.body.style.backgroundImage = "url(" + gifs[current_gif].src + ")";
   // TODO: use duration
   setTimeout(loadNextGif, 10000);
