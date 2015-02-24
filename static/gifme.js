a2c4023ad6c3656e0db271286786b2755ce4bc92
Ted Mielczarek <ted@mielczarek.org>
2015-02-24 09:31:13 -0500

    Use webm versions of imgur gifs in Gecko

--- static/gifme.js
+++ static/gifme.js
@@ -1,3 +1,4 @@
+var useVideoBG = !!document.mozSetImageElement;
 var gifs = [];
 var current_gif = -1;
 var queue = [];
@@ -19,8 +20,19 @@ es.addEventListener("ping", function(e) {
 function preloadNextImage() {
   if (queue.length == 0)
     return;
-  loading = new Image();
-  loading.onload = function() {
+  var src = queue[0];
+  var event = "load";
+  if (src.substr(0, 18) == "http://i.imgur.com" && useVideoBG)  {
+    src = src.substr(0, src.length - 4) + ".webm";
+    loading = document.createElement("video");
+    loading.autoplay = true;
+    loading.loop = true;
+    event = "canplaythrough";
+  } else {
+    loading = new Image();
+  }
+  loading.addEventListener(event, function doneloading() {
+    loading.removeEventListener(event, doneloading);
     queue.shift();
     console.log("loaded %s", loading.src);
     if (!loading.displayed) {
@@ -28,8 +40,8 @@ function preloadNextImage() {
     }
     loading = null;
     setTimeout(preloadNextImage, 0);
-  };
-  loading.src = queue[0];
+  });
+  loading.src = src;
   console.log("preloading %s", loading.src);
   if (current_gif == -1) {
     gifs.push(loading);
@@ -48,8 +60,14 @@ function loadNextGif() {
     }
     current_gif = (current_gif + 1) % gifs.length;
   }
-  console.log("using gif %s", gifs[current_gif].src);
-  document.body.style.backgroundImage = "url(" + gifs[current_gif].src + ")";
+  var gif = gifs[current_gif];
+  console.log("using gif %s", gif.src);
+  if (gif instanceof HTMLImageElement) {
+    document.body.style.backgroundImage = "url(" + gifs[current_gif].src + ")";
+  } else {
+    document.mozSetImageElement("videobg", gif);
+    document.body.style.backgroundImage = "-moz-element(#videobg)";
+  }
   // TODO: use duration
   setTimeout(loadNextGif, 10000);
 }
