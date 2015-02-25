8b47e91a778d6d568029e7dcf132a7b9811a5a93
Ted Mielczarek <ted@mielczarek.org>
2015-02-25 08:00:30 -0500

    Multiple changes: - Limit the number of gifs in rotation - Use gif duration instead of hardcoding 10 seconds - Try to loop gifs at least twice

--- static/gifme.js
+++ static/gifme.js
@@ -1,14 +1,20 @@
 var useVideoBG = !!document.mozSetImageElement;
 var gifs = [];
+// Don't keep more than this many gifs in rotation.
+var MAX_GIFS = 10;
+// If a gif is shorter than this let it loop until it plays at least
+// this long.
+var MIN_DURATION = 5000;
+// Also let gifs loop at least this many times.
+var MIN_LOOPS = 2;
 var current_gif = -1;
 var queue = [];
 var loading;
 var es = new EventSource("/feed");
 es.addEventListener("gif", function(e) {
   var data = JSON.parse(e.data);
-  console.log("got img url %s", data.url);
-  // TODO: use duration
-  queue.push(data.url);
+  console.log("got img url %s, duration %d", data.url, data.duration);
+  queue.push(data);
   if (queue.length == 1) {
     preloadNextImage();
   }
@@ -20,7 +26,7 @@ es.addEventListener("ping", function(e) {
 function preloadNextImage() {
   if (queue.length == 0)
     return;
-  var src = queue[0];
+  var src = queue[0].url;
   var event = "load";
   if (src.substr(0, 18) == "http://i.imgur.com" && useVideoBG)  {
     src = src.substr(0, src.length - 4) + ".mp4";
@@ -31,12 +37,19 @@ function preloadNextImage() {
   } else {
     loading = new Image();
   }
+  // HTMLMediaElement has .duration...
+  loading.duration_ = queue[0].duration;
   loading.addEventListener(event, function doneloading() {
     loading.removeEventListener(event, doneloading);
     queue.shift();
-    console.log("loaded %s", loading.src);
+    console.log("loaded %s, duration %d", loading.src, loading.duration_);
+    loading.loaded = true;
     if (!loading.displayed) {
       gifs.push(loading);
+      // Prune the set of gifs
+      while (gifs.length > MAX_GIFS) {
+        gifs.shift();
+      }
     }
     loading = null;
     setTimeout(preloadNextImage, 0);
@@ -54,8 +67,9 @@ function loadNextGif() {
   if (current_gif == -1) {
     current_gif = 0;
   } else {
-    if (gifs.length == 1) {
-      // Only one gif, don't bother changing.
+    if (gifs.length == 1 || !gifs[current_gif].loaded) {
+      // Only one gif or it hasn't fully loaded, don't bother changing.
+      setTimeout(loadNextGif, Math.min(5000, gifs[current_gif].duration_));
       return;
     }
     current_gif = (current_gif + 1) % gifs.length;
@@ -68,6 +82,12 @@ function loadNextGif() {
     document.mozSetImageElement("videobg", gif);
     document.body.style.backgroundImage = "-moz-element(#videobg)";
   }
-  // TODO: use duration
-  setTimeout(loadNextGif, 10000);
+  // Let gifs loop at least MIN_LOOPS times, but maybe more if they're short.
+  var duration = 0;
+  var loops = 0;
+  while (duration < MIN_DURATION || loops < MIN_LOOPS) {
+    duration += gif.duration_;
+    loops++;
+  }
+  setTimeout(loadNextGif, duration);
 }
